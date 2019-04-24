8a23bea0ec93f9fc19ea058720cce48d3047e34c
Retno Ayu Adiati <retno_ayu@hotmail.com>
2019-04-24 14:11:51 +0200

    add arrays file

---
+++ arrays/arrays.js
@@ -0,0 +1,10 @@
+var todos = ['item 1', 'item 2', 'item 3']; // Store arrays
+
+todos.push('item 4'); // Add arrays
+todos.push('item 5'); // Add another arrays
+
+todos[0] = 'item 1 updated'; // Change arrays
+
+todos.splice(1, 2); // Delete arrays  ---> start from item 2, deleting 2 items.
+
+console.log('Things to do:', todos); // Display arrays
